5c43c750b9b161881f05be22983f4752a6743d9a
Julian Dunskus <julian.dunskus@gmail.com>
2020-11-21 04:55:38 +0100

    group role claim entries

--- src/functions/role-claim.ts
+++ src/functions/role-claim.ts
@@ -1,57 +1,104 @@
-import { Client, EmojiResolvable, GuildEmoji, GuildMember, Message, Role, TextChannel } from "discord.js";
+import { Client, GuildEmoji, GuildMember, Message, MessageEmbed, Role, TextChannel } from "discord.js";
+import { delay } from "../common";
+import { channelIDs, serverID } from "../config";
 
-const serverID = '767139170350792704';
-const roleClaimChannelID = '767140016676798524';
+class RoleGroup {
+	constructor(
+		readonly title: string,
+		readonly entries: RawRoleEntry[],
+	) { }
 
-const emojis: [string, string][] = [
-	['Minecraft', 'Minecraft'],
-	['Valorant', 'Valorant'],
-	['AmongUs', 'Among Us'],
-	['rocketleague', 'Rocket League'],
-	['leagueoflegends', 'League of Legends'],
-	['Maennlich', 'Male'],
-	['Weiblich', 'Female'],
-	['Divers', 'Divers'],
-	['FSK12', 'FSK 12'],
-	['FSK16', 'FSK 16'],
-	['FSK18', 'FSK 18'],
-	['FSK21', 'FSK 21'],
-	['Twitch', 'Streamer'],
-	['gay', 'Gay'],
+	static raw(title: string, entries: [string, string][]): RoleGroup {
+		return new RoleGroup(title, entries.map(([emoji, role]) => new AbstractRoleEntry(emoji, role)));
+	}
+}
+
+class AbstractRoleEntry<E, R> {
+	constructor(
+		readonly emoji: E,
+		readonly role: R,
+	) { }
+}
+
+type RawRoleEntry = AbstractRoleEntry<string, string>;
+type RoleEntry = AbstractRoleEntry<GuildEmoji, Role>;
+
+const emojis: RoleGroup[] = [
+	RoleGroup.raw("Games", [
+		['Minecraft', 'Minecraft'],
+		['Valorant', 'Valorant'],
+		['AmongUs', 'Among Us'],
+		['rocketleague', 'Rocket League'],
+		['leagueoflegends', 'League of Legends'],
+	]),
+	RoleGroup.raw("Gender", [
+		['Maennlich', 'Male'],
+		['Weiblich', 'Female'],
+		['Divers', 'Divers'],
+	]),
+	RoleGroup.raw("Sexuality", [
+		['gay', 'Gay'],
+	]),
+	RoleGroup.raw("Age", [
+		['FSK12', 'FSK 12'],
+		['FSK16', 'FSK 16'],
+		['FSK18', 'FSK 18'],
+		['FSK21', 'FSK 21'],
+	]),
+	RoleGroup.raw("Miscellaneous", [
+		['Twitch', 'Streamer'],
+	]),
 ];
 
-export function handleRoleClaims(client: Client) {
+export async function handleRoleClaims(client: Client) {
 	const guild = client.guilds.resolve(serverID)!;
-	const roleClaimChannel = guild.channels.resolve(roleClaimChannelID) as TextChannel;
+	const roleClaimChannel = guild.channels.resolve(channelIDs.roleClaim) as TextChannel;
 
-	type RoleEntry = { emoji: GuildEmoji, role: Role };
-	const resolvedEmojis: RoleEntry[] = emojis
-		.map(([emojiName, roleName]) => ({
-			emoji: guild.emojis.cache.find(emoji => emoji.name === emojiName)!,
-			role: guild.roles.cache.find(role => role.name === roleName)!,
-		}));
-	const optionsText = resolvedEmojis
-		.map(roleEntry => `${roleEntry.emoji} = ${roleEntry.role}`)
-		.join("\n");
-	const emojiText = `Use the reactions on this message to opt-in and out of roles.\n\n${optionsText}`;
+	const resolvedEntries: RoleEntry[][] = emojis.map(group =>
+		group.entries.map(raw => new AbstractRoleEntry(
+			guild.emojis.cache.find(emoji => emoji.name === raw.emoji)!,
+			guild.roles.cache.find(role => role.name === raw.role)!,
+		))
+	);
 
-	ensureNthMessage(roleClaimChannel, 0, emojiText, resolvedEmojis.map(x => x.emoji));
+	ensureNthMessage(roleClaimChannel, 1, "Use the reactions on these messages to opt into and out of roles.");
+
+	for (const groupIndex of emojis.keys()) {
+		const groupEntries = resolvedEntries[groupIndex];
+		const optionsText = groupEntries
+			.map(roleEntry => `${roleEntry.emoji} = ${roleEntry.role.name}`)
+			.join("\n");
+		const embed = new MessageEmbed();
+		embed.setTitle(emojis[groupIndex].title);
+		embed.setDescription(optionsText);
+
+		// async
+		ensureNthMessage(
+			roleClaimChannel,
+			groupIndex + 2, embed,
+			groupEntries.map(x => x.emoji)
+		);
+		await delay(100);
+	}
+
+	const flatEntries = Array<RoleEntry>().concat(...resolvedEntries);
 
 	function registerHandler(
 		event: string,
 		callback: (member: GuildMember, role: Role) => void
 	) {
 		client.on(event, (reaction, user) => {
-			if (reaction.message.channel.id !== roleClaimChannelID) return;
-			if (user.id === '775516814162788352') return;
+			if (reaction.message.channel.id !== channelIDs.roleClaim) return;
+			if (user.bot) return;
 
 			const emoji = reaction.emoji.id;
 
 			const guild = reaction.message.guild!;
-			const role = resolvedEmojis.find(entry => entry.emoji.id === emoji)!.role;
+			const roleEntry = flatEntries.find(entry => entry.emoji.id === emoji);
+			if (roleEntry === undefined) return;
 
 			const member = guild.members.resolve(user.id)!;
-			callback(member, role);
+			callback(member, roleEntry.role);
 		});
 	}
 
@@ -59,23 +106,42 @@ export function handleRoleClaims(client: Client) {
 	registerHandler('messageReactionRemove', (member, role) => member.roles.remove(role));
 }
 
-async function ensureNthMessage(channel: TextChannel, n: number, text: string, reactions: GuildEmoji[] = []) {
+/**
+ * 1-based, i.e. n = 1 means the first message
+ */
+async function ensureNthMessage(
+	channel: TextChannel,
+	n: number, message: string | MessageEmbed,
+	reactions: GuildEmoji[] = []
+) {
 	const messages = await channel.messages.fetch();
-	let message: Message;
+	let sentMessage: Message;
 
-	if (messages.size <= n) {
-		message = await channel.send(text);
+	if (messages.size < n) {
+		sentMessage = await channel.send(message);
 	} else {
-		message = messages.array()[n];
-		message.edit(text);
+		sentMessage = messages.array()[messages.size - n];
+		sentMessage.edit(message);
 	}
 
-	addReactions(message, reactions);
-}
+	const existingReactions = sentMessage.reactions.cache.array();
+	// find first index where existing reaction doesn't match desired ones
+	let nonMatchingStart = existingReactions
+		.findIndex((existing, index) => existing.emoji !== reactions[index]);
+	if (nonMatchingStart < 0) {
+		nonMatchingStart = existingReactions.length;
+	}
 
-function addReactions(message: Message, reactions: EmojiResolvable[]) {
-	const reaction = reactions.shift();
-	if (reaction === undefined) return;
-	message.react(reaction);
-	setTimeout(() => addReactions(message, reactions), 750);
+	// remove non-matching existing reactions
+	const toRemove = existingReactions.slice(nonMatchingStart);
+	for (const existing of toRemove) {
+		await existing.remove();
+		await delay(500);
+	}
+
+	const toAdd = reactions.slice(nonMatchingStart);
+	for (const reaction of toAdd) {
+		await sentMessage.react(reaction);
+		await delay(500);
+	}
 }
