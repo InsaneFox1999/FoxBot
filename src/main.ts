c66ebe03bb4a6a61287ec283bf0a6d4165362eda
Julian Dunskus <julian.dunskus@gmail.com>
2020-11-22 04:45:13 +0100

    error/rejection logging

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { handleWelcomes } from './functions/welcome';
 import { loadCommands } from './functions/commands';
 import { handleVoiceChannels } from './functions/voice';
 import { handleRoleClaims } from './functions/role-claim';
+import * as fs from 'fs-extra';
+import * as path from 'path';
 
 const client = new Client();
 client.login(token);
@@ -16,4 +18,28 @@ client.on('ready', () => {
 	loadCommands(client);
 	handleRoleClaims(client);
 	handleVoiceChannels(client);
+
+	throw { reason: "oh no!" };
+});
+
+function writeLogFile<T>(prefix: string, contents: T) {
+	fs.ensureDirSync("log");
+	const errorFile = path.join("log", `${prefix}-${new Date().toISOString()}.json`);
+	fs.writeJSONSync(errorFile, contents);
+}
+
+process.on('unhandledRejection', (error, _promise) => {
+	console.log("unhandled rejection! D:");
+	console.dir(error);
+	writeLogFile('unhandledRejection', {
+		error
+	});
+});
+
+process.on('uncaughtException', (error) => {
+	console.log("uncaught exception! D:");
+	console.log(error);
+	writeLogFile('uncaughtException', {
+		error, errorString: error.toString(), stack: error.stack
+	});
 });
